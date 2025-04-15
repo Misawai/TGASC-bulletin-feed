cfeb64d8d8b814b3403fd71b6e24e6814f08e2bf
Misawai <69676474+Misawai@users.noreply.github.com>
2025-04-15 18:39:39 +0800

    updateLatest: init latest bulletin number updater

---
+++ updateLatest.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# updateLatest.sh
+# Keep the latest bulletin number up to date.
+
+set -e
+
+# read the latest bulletin
+latestnum=$(<./latest)
+
+statusCode=$(curl -s -I https://www.scout.org.tw/news_detail/${latestnum} | grep "^HTTP\/" | awk '{print $2}')
+
+if [ "$statusCode" == "404" ]; then
+	echo "Have reached to the latest bulletin."
+	echo "true" >./status
+	exit 0;
+else 
+	((latestnum++))
+	echo "$latestnum" >./latest
+	echo "false" >./status;
+fi
